bf4caaa8cc02ab030da867de106ea080f2856d69
wali55 <walisantunu@gmail.com>
2023-08-20 20:13:49 +0600

    tour guide with mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/AppBar';
+import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import cities from './data.json';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              key={city.id}
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour) => (
+                <TourCard tour={tour} key={tour.id} />
+              ))}
+            </Grid>
+          </>
+        ))}
+      </Container>
     </div>
   );
 }
